9258a5bd9fb992da64fcf4f4fb2a15faea9db600
faizaansheikh <princefaizan800@gmail.com>
2026-02-11 15:33:38 +0500

    fs

--- app/components/ui/Nav.tsx
+++ app/components/ui/Nav.tsx
@@ -15,7 +15,7 @@ const Nav = () => {
     const path = usePathname()
     const router = useRouter()
     const [open, setOpen] = useState(false);
-    const [flag, setFlag] = useState(false);
+    const [flag, setFlag] = useState(true);
     const [username, setUsername] = useState('')
     const [userrole, setUserRole] = useState('')
     const handleLogin = () => router.push('/auth/login')
@@ -31,15 +31,15 @@ const Nav = () => {
         removeAuthToken()
         // window.location.href = '/home'
         router.push('/home')
-        
+        setUserRole('')
 
     }
     useEffect(() => {
-        
+
         try {
             const userInfo = getUser()
             if (userInfo) {
-             
+
                 setUsername(userInfo?.name || '');
                 setUserRole(userInfo?.role || '');
             }
@@ -53,7 +53,7 @@ const Nav = () => {
 
 
 
-  
+
     const menu = [
         { title: 'Home', link: 'home' },
         { title: 'Programs', link: 'programs' },
@@ -64,77 +64,78 @@ const Nav = () => {
 
     return (
         <>
-            <div
-                className={`fixed top-0 left-0 right-0 z-[2000]  flex justify-between items-center px-4 transition-all duration-300 px-4 lg:px-32 
+             <div
+                    className={`fixed top-0 left-0 right-0 z-[2000]  flex justify-between items-center px-4 transition-all duration-300 px-4 lg:px-32 
                     }`}
-                style={{ backgroundColor: 'white' }}
-            >
-                <div onClick={handleHome} className='cursor-pointer text-2xl'>
-                    <Image
-                        src='/logo.png'
-                        alt='logo'
-                        width={100}
-                        height={100}
-
-                    />
-                </div>
-
-                <ul className="hidden md:flex items-center justify-center" >
-                    {menu.map((x, i) => (
-                        <Link href={'/'} key={i}>
-                            <li
-                                onClick={(e) => {
-                                    const arr = path?.split('/')
-                                    if (arr?.includes('products') && x.link === 'home') {
-                                        router.push('/home')
-                                    } else {
-                                        e.preventDefault();
-                                        const section = document.getElementById(x.link);
-                                        section?.scrollIntoView({ behavior: "smooth" });
-                                    }
-
-
-                                }}
-                                className={`list-none px-4 text-[17px] relative nav-list cursor-pointer text-grey text-gray-800 cursor-pointer transition-all duration-400 hover:text-primary
-                                }`}
-                            // onClick={() => handleLinks(x)}
-                            >
-                                {x.title}
-                            </li>
-                        </Link >
-
-                    ))}
-                    {userrole === 'student' ?
-                        <>
-                            <span className='text-primary text-lg px-0 cursor-pointer' onClick={handleBrowse}>Browse Products</span>
-                            <span className='text-xl px-3'>{username}</span>
-                            <Popover content={<div className='text-lg '>
-                                <p className='p-1 cursor-pointer hover:bg-gray-200'>Edit profile</p>
-                                <p onClick={handleLogout} className='p-1 cursor-pointer hover:bg-gray-200'>Logout</p>
-                            </div>} title="" trigger="click" placement='bottom'>
-                                <Avatar className='cursor-pointer' size="large" icon={<UserOutlined />} />
-                            </Popover>
-
-                        </>
-
-                        :
-                        <>
-                            <span onClick={handleLogin} className='text-primary cursor-pointer h-15 w-auto px-4 flex items-center  hover:bg-[#e9e5e5] duration-200'>Login</span>
-                            <XButton Click={handleSignup} label='Sign up' />
-                        </>
-                    }
-                </ul>
-
-
-
-                <div
-                    className="sm:flex justify-center items-center md:hidden cursor-pointer"
-                    onClick={showDrawer}
+                    style={{ backgroundColor: 'white' }}
                 >
-                    <GiHamburgerMenu style={{ color: 'black', fontSize: '20px' }} color={'white'} size={20} />
-                    {/* <GiHamburgerMenu color={'white'} size={20} /> */}
+                    <div onClick={handleHome} className='cursor-pointer text-2xl'>
+                        <Image
+                            src='/logo.png'
+                            alt='logo'
+                            width={100}
+                            height={100}
+
+                        />
+                    </div>
+
+                    <ul className="hidden md:flex items-center justify-center" >
+                        {menu.map((x, i) => (
+                            <Link href={'/'} key={i}>
+                                <li
+                                    onClick={(e) => {
+                                        const arr = path?.split('/')
+                                        if (arr?.includes('products') && x.link === 'home') {
+                                            router.push('/home')
+                                        } else {
+                                            e.preventDefault();
+                                            const section = document.getElementById(x.link);
+                                            section?.scrollIntoView({ behavior: "smooth" });
+                                        }
+
+
+                                    }}
+                                    className={`list-none px-4 text-[17px] relative nav-list cursor-pointer text-grey text-gray-800 cursor-pointer transition-all duration-400 hover:text-primary
+                                }`}
+                                // onClick={() => handleLinks(x)}
+                                >
+                                    {x.title}
+                                </li>
+                            </Link >
+
+                        ))}
+                        {userrole === 'student' ?
+                            <>
+                                <span className='text-primary text-lg px-0 cursor-pointer' onClick={handleBrowse}>Browse Products</span>
+                                <span className='text-xl px-3'>{username}</span>
+                                <Popover content={<div className='text-lg '>
+                                    <p className='p-1 cursor-pointer hover:bg-gray-200'>Edit profile</p>
+                                    <p onClick={handleLogout} className='p-1 cursor-pointer hover:bg-gray-200'>Logout</p>
+                                </div>} title="" trigger="click" placement='bottom'>
+                                    <Avatar className='cursor-pointer' size="large" icon={<UserOutlined />} />
+                                </Popover>
+
+                            </>
+
+                            :
+                            <>
+                                <span onClick={handleLogin} className='text-primary cursor-pointer h-15 w-auto px-4 flex items-center  hover:bg-[#e9e5e5] duration-200'>Login</span>
+                                <XButton Click={handleSignup} label='Sign up' />
+                            </>
+                        }
+                    </ul>
+
+
+
+                    <div
+                        className="sm:flex justify-center items-center md:hidden cursor-pointer"
+                        onClick={showDrawer}
+                    >
+                        <GiHamburgerMenu style={{ color: 'black', fontSize: '20px' }} color={'white'} size={20} />
+                        {/* <GiHamburgerMenu color={'white'} size={20} /> */}
+                    </div>
                 </div>
-            </div>
+            
 
             <Drawer
                 title={
